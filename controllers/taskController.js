cef81acac8ab60a0d66ff4f700106ffbb8bd0571
masrudmubarok <masrud.mubarok21@gmail.com>
2025-02-01 10:53:23 +0700

    Update & delete task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -38,3 +38,59 @@ exports.getTasks = asyncHandler(async (req, res) => {
         message: 'Tasks retrieved successfully',
     });
 });
+
+/**
+ * @desc Update a task
+ * @route PUT /tasks/:id
+ * @access Public
+ */
+exports.updateTask = asyncHandler(async (req,res) => {
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+
+    const task = await Task.findById(id);
+
+    if (!task) {
+        return res.status(404).json({
+            success: false,
+            message: 'Task not found',
+        });
+    }
+
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.status = status || task.status;
+
+    const updateTask = await task.save();
+
+    res.status(200).json({
+        success: true,
+        data: updateTask,
+        message: 'Task updated successfully',
+    });
+});
+
+/**
+ * @desc Delete a task
+ * @route DELETE /tasks/:id
+ * @access Public
+ */
+exports.deleteTask = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const task = await Task.findById(id);
+
+    if (!task) {
+        return res.status(404).json({
+            success: false,
+            message: 'Task not found',
+        });
+    }
+
+    await task.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: 'Task deleted successfully',
+    });
+});
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getTasks, createTask } = require('../controllers/taskController');
+const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
 
 router.route('/')
     .get(getTasks)  // Mengambil semua task
     .post(createTask);  // Membuat task baru
+
+router.route('/:id')
+    .put(updateTask)  // Mengupdate task berdasarkan ID
+    .delete(deleteTask);  // Menghapus task berdasarkan ID
 
 module.exports = router;
